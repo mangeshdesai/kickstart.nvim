3e18602ce0671d4ff7bc25c8028bd553e3f969d1
Mangesh Desai <mangeshdesai@live.com>
2025-07-14 16:52:32 +0000

    add config for toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,6 +3,20 @@ return {
   version = '*',
   opts = {
     open_mapping = [[<c-\>]],
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    persist_mode = true,
+    close_on_exit = true,
     direction = 'float',
+    float_opts = {
+      border = 'curved',
+      winblend = 3,
+      highlights = {
+        border = 'Normal',
+        background = 'Normal',
+      },
+    },
   },
 }
